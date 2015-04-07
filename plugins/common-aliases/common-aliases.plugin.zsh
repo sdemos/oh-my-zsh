021f18b71869737033f97046ba9a078e4b6ab718
Stephen Demos <stphndemos@gmail.com>
2015-04-07 10:27:17 -0400

    got rid of some not useful aliases
    they were getting in my way when I wanted to type things like M

--- plugins/common-aliases/common-aliases.plugin.zsh
+++ plugins/common-aliases/common-aliases.plugin.zsh
@@ -21,13 +21,7 @@ alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
 alias t='tail -f'
 
 # Command line head / tail shortcuts
-alias -g H='| head'
-alias -g T='| tail'
-alias -g G='| grep'
-alias -g L="| less"
-alias -g M="| most"
 alias -g LL="2>&1 | less"
-alias -g CA="2>&1 | cat -A"
 alias -g NE="2> /dev/null"
 alias -g NUL="> /dev/null 2>&1"
 alias -g OUT="2>&1 | tee build.out"
